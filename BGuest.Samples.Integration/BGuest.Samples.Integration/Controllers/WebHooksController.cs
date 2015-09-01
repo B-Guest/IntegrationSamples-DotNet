70a9c63d5ad539edc03dfa7c419ce2d0e9777148
Tomás Oliveira <tomas@b-guest.com>
2015-09-01 12:07:24 +0100

    new file:   BGuest.Samples.Integration/BGuest.Samples.Integration.sln 	new file:   BGuest.Samples.Integration/BGuest.Samples.Integration/App_Start/WebApiConfig.cs 	new file:   BGuest.Samples.Integration/BGuest.Samples.Integration/BGuest.Samples.Integration.csproj 	new file:   BGuest.Samples.Integration/BGuest.Samples.Integration/Controllers/WebHooksController.cs 	new file:   BGuest.Samples.Integration/BGuest.Samples.Integration/Global.asax 	new file:   BGuest.Samples.Integration/BGuest.Samples.Integration/Global.asax.cs 	new file:   BGuest.Samples.Integration/BGuest.Samples.Integration/Properties/AssemblyInfo.cs 	new file:   BGuest.Samples.Integration/BGuest.Samples.Integration/Web.Debug.config 	new file:   BGuest.Samples.Integration/BGuest.Samples.Integration/Web.Release.config 	new file:   BGuest.Samples.Integration/BGuest.Samples.Integration/Web.config 	new file:   BGuest.Samples.Integration/BGuest.Samples.Integration/packages.config

---
+++ BGuest.Samples.Integration/BGuest.Samples.Integration/Controllers/WebHooksController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Web.Http;
+
+namespace BGuest.Samples.Integration.Controllers
+{
+    [RoutePrefix("webhooks")]
+    public class WebHooksController : ApiController
+    {
+
+        [Route("requestchanges")]
+        [HttpPost]
+        public HttpResponseMessage Post(RequestChangesModel model)
+        {
+            Debug.WriteLine($"Request {model.Operation}:");
+            Debug.WriteLine($"RequestId: {model.RequestId}, SubService: '{model.SubServiceName}', " +
+                            $"SubServiceType: '{model.SubServiceTypeName}', " +
+                            $"PointOfInterest: '{model.PointOfInterestName}'.");
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+    }
+
+    public class RequestChangesModel
+    {
+        [Required, DataMember(IsRequired = true)]
+        public int RequestId { get; set; }
+
+        public string SubServiceName { get; set; }
+
+        public string SubServiceTypeName { get; set; }
+
+        public string PointOfInterestName { get; set; }
+
+        [Required, DataMember(IsRequired = true)]
+        public string Operation { get; set; }
+    }
+}
